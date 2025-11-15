975a64b822fce83f103be78b9675dde49b3a213f
logan416ah-cloud <logan416ah@gmail.com>
2025-11-15 00:50:37 -0500

    Update APInytimes.py

--- APInytimes.py
+++ APInytimes.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 from datetime import datetime
 from getpass import getpass
+from pathlib import Path
 
 baseurl = 'https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json'
 
@@ -56,6 +57,9 @@ response_data = main_request(baseurl)
 
 try:
     data = parse_json(response_data)
+    if data.empty:
+        print("No articles returned.")
+        exit()
 except KeyError as e: # Catch JSON structure issues
     print(f"Unexpected JSON format, missing key: {e}")
     exit()
@@ -65,9 +69,14 @@ print(data)
 choice = input("\nWould you like to save as CSV (y/n)? ").lower()
 
 if choice == 'y':
+    folder = Path("NYT_Data")
+    folder.mkdir(exist_ok=True)
+
     todays_date = datetime.now().strftime("%Y-%m-%d")
     filename = input("\nSave file as? ")
-    data.to_csv(f'{filename}_{todays_date}.csv', index=False)
-    print(f"Saved as {filename}_{todays_date}.csv")
+    file_path = folder / f"{filename}_{todays_date}.csv"
+
+    data.to_csv(file_path, index=False)
+    print(f"Saved as {file_path}")
 else:
     print("Closing program.")
